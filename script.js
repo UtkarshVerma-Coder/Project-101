a9bc6026e3234ad41c4fa5d1877c64d0d8342fa1
UtkarshVerma-Coder <utkarshverma001@outlook.com>
2022-09-28 01:58:27 +0530

    added ui

---
+++ script.js
@@ -0,0 +1,36 @@
+var insta = document.getElementById("insbut");
+var Twitter = document.getElementById("gitbut");
+var github = document.getElementById("twitbut");
+
+
+var titlebar = document.getElementById("title");
+var username = document.getElementById("username");
+
+
+var followbutton = document.getElementById("followbutt");
+
+
+// Make It Insta
+
+var text = "Follow"
+
+function makeitinsta() {
+    username.innerHTML = "utkarshverma___"
+    var instalink = text.link("https://www.instagram.com/utkarshverma___/")
+    followbutton.innerHTML = instalink;
+}
+function makeittwitter() {
+    username.innerHTML = "@Utkarsh_Verma_"
+    var githublink = text.link("https://twitter.com/Utkarsh_Verma_")
+    followbutton.innerHTML = githublink;
+
+}
+function makeitgithub() {
+    username.innerHTML = "UtkarshVerma-Coder"
+    var twittelink = text.link("https://github.com/UtkarshVerma-Coder")
+    followbutton.innerHTML = twittelink;
+
+}
+
+
+
